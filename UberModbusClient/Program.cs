a815be0e4c36ff54cd1175b0496b927faa98fa7b
Anbarasu Angappan <anburvhs@hotmail.com>
2018-08-23 19:23:45 +0530

    Added the additional Function

--- UberModbusClient/Program.cs
+++ UberModbusClient/Program.cs
@@ -14,79 +14,166 @@ namespace UberModbusClient
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Please Enter The Meter IP Address:");
-            modbusClient = new ModbusClient(Console.ReadLine(), 502);
-            program = new Program();
-            modbusClient.Connect();
-            if (modbusClient.Connected)
+            try
             {
-                Console.WriteLine("Connected...");
-                Console.WriteLine("Enter 1 to Read the ReadHoldingRegisters");
-                Console.WriteLine("Enter 2 to Read the ReadInputRegisters");
-                string function = Console.ReadLine();
-                int a = Convert.ToInt32(function);
-                switch (a)
+                Console.WriteLine("Please Enter The Meter IP Address:");
+                modbusClient = new ModbusClient(Console.ReadLine(), 502);
+                program = new Program();
+                modbusClient.Connect();
+                if (modbusClient.Connected)
                 {
-                    case 1:                       
-                        program.ReadHoldingRedister();
-                        break;
-                    case 2:                      
-                        program.ReadInputRegister();
-                        break;
-
-                    default:
-                        Console.WriteLine("Default case");
-                        break;
-                }  
+                    Console.WriteLine("Connected...");
+                    Console.WriteLine("Enter 1: To Read the ReadHoldingRegisters");
+                    Console.WriteLine("Enter 2: To Read the ReadInputRegisters");
+                    Console.WriteLine("Enter 3: To Read the ReadDiscreteInput");
+                    Console.WriteLine("Enter 4: To Read the ReadCoil");
+                    string function = Console.ReadLine();
+                    int inputKeyFunction = Convert.ToInt32(function);
+                    switch (inputKeyFunction)
+                    {
+                        case 1:
+                            program.ReadHoldingRegister();
+                            break;
+                        case 2:
+                            program.ReadInputRegister();
+                            break;
+                        case 3:
+                            program.ReadDiscreteInput();
+                            break;
+                        case 4:
+                            program.ReadCoil();
+                            break;
+                        default:
+                            Console.WriteLine("Default case");
+                            break;
+                    }
+                }
+                else
+                    Console.WriteLine("Not Connected");
             }
-            else
-                Console.WriteLine("Not Connected");
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
         }
 
-        public void ReadHoldingRedister()
+        public void ReadHoldingRegister()
         {
-            Console.WriteLine("Please Enter The Starting Address:");
-            string holdingregister = Console.ReadLine();
-            int register = Convert.ToInt32(holdingregister);
-
-            //Console.WriteLine("Please Enter The Quantity Length:");
-            //string holdingregisterlength = Console.ReadLine();
-            //int registerlength = Convert.ToInt32(holdingregisterlength);
-
-            int[] readHoldingRegisters = modbusClient.ReadHoldingRegisters(register, 50);//40017               
-            //int[] readInputRegisters = modbusClient.ReadInputRegisters(13, 10);
-            //bool[] readDiscreteInputs  = modbusClient.ReadDiscreteInputs(13,10);    
-
-            for (int i = 0; i < readHoldingRegisters.Length; i++)
+            try
             {
-                Decimal _toDecimal = Convert.ToDecimal(readHoldingRegisters[i]);                
-                Console.WriteLine((register) + ":" + "<" + _toDecimal + ">");//Console.WriteLine("Value Of HoldingRegister" + (register) + ":" + "<" + _toDecimal + ">");
-                register++;
+                Console.WriteLine("Please Enter The Starting Address of ReadHoldingRegister:");
+                string holdingregister = Console.ReadLine();
+                int register = Convert.ToInt32(holdingregister);
+                //Console.WriteLine("Please Enter The Quantity Length:");
+                //string holdingregisterlength = Console.ReadLine();
+                //int registerlength = Convert.ToInt32(holdingregisterlength);
+                int[] readHoldingRegisters = modbusClient.ReadHoldingRegisters(register, 50);//40017               
+                //int[] readInputRegisters = modbusClient.ReadInputRegisters(13, 10);
+                //bool[] readDiscreteInputs  = modbusClient.ReadDiscreteInputs(13,10);    
+                if (readHoldingRegisters != null)
+                {
+                    for (int i = 0; i < readHoldingRegisters.Length; i++)
+                    {
+                        Decimal _toDecimal = Convert.ToDecimal(readHoldingRegisters[i]);
+                        Console.WriteLine((register) + ":" + "<" + _toDecimal + ">");//Console.WriteLine("Value Of HoldingRegister" + (register) + ":" + "<" + _toDecimal + ">");
+                        register++;
+                    }
+                }
+                //for (int i = 0; i < readInputRegisters.Length; i++)
+                //{
+                //    Console.WriteLine("Value of the InputRegisters" + (i + 1) + ":" + readInputRegisters[i]);
+                //}
+
+                //for (int i = 0; i < readDiscreteInputs.Length; i++)
+                //{
+                //    Console.WriteLine("Value of the ReadDiscreteInputs" + (i + 1) + ":" + readDiscreteInputs[i]);
+                //}
+                modbusClient.Disconnect();
+                Console.ReadKey();
             }
-
-            //for (int i = 0; i < readInputRegisters.Length; i++)
-            //{
-            //    Console.WriteLine("Value of the InputRegisters" + (i + 1) + ":" + readInputRegisters[i]);
-            //}
-
-            //for (int i = 0; i < readDiscreteInputs.Length; i++)
-            //{
-            //    Console.WriteLine("Value of the ReadDiscreteInputs" + (i + 1) + ":" + readDiscreteInputs[i]);
-            //}
-
-            modbusClient.Disconnect();
-            Console.ReadKey();
-
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
         }
 
         public void ReadInputRegister()
         {
-
+            try
+            {
+                Console.WriteLine("Please Enter The Starting Address of Input Register:");
+                string holdingregister = Console.ReadLine();
+                int register = Convert.ToInt32(holdingregister);
+                int[] readInputRegisters = modbusClient.ReadInputRegisters(register, 10);
+                if (readInputRegisters != null)
+                {
+                    for (int i = 0; i < readInputRegisters.Length; i++)
+                    {
+                        Decimal _toDecimal = Convert.ToDecimal(readInputRegisters[i]);
+                        Console.WriteLine((register) + ":" + "<" + _toDecimal + ">");
+                        register++;
+                    }
+                }
+                modbusClient.Disconnect();
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
         }
 
+        public void ReadDiscreteInput()
+        {
+            try
+            {
+                Console.WriteLine("Please Enter The Starting Address of DiscreteInput:");
+                string discreteInput = Console.ReadLine();
+                int input = Convert.ToInt32(discreteInput);
+
+                bool[] readDiscreteInputs = modbusClient.ReadDiscreteInputs(input, 10);
+                if (readDiscreteInputs != null)
+                {
+                    for (int i = 0; i < readDiscreteInputs.Length; i++)
+                    {
+                        Console.WriteLine((input) + ":" + "<" + readDiscreteInputs[i] + ">");
+                        input++;
+                    }
+                }
+                modbusClient.Disconnect();
+                Console.ReadLine();
+
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public void ReadCoil()
+        {
+            try
+            {
+                Console.WriteLine("Please Enter the Starting Adderess of Read Coils");
+                string readCoil = Console.ReadLine();
+                int coil = Convert.ToInt32(readCoil);
+
+                bool[] readCoils = modbusClient.ReadCoils(coil, 10);
+                if(readCoils != null)
+                {
+                    for (int i = 0; i < readCoils.Length; i++)
+                    {
+                        Console.WriteLine((coil) + ":" + "<" + readCoils[i] + ">");
+                        coil++;
+                    }
+                }
+                modbusClient.Disconnect();
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
     }
-
-    
-
-    
 }
